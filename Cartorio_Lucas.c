56050e6bb86cac8d5c8304859f300b0eb74c5fe3
Lucas Vinicius <60452441+LucasAdbr@users.noreply.github.com>
2024-04-06 01:30:44 -0300

    opção de saida
    colocada a opção de saida do sistema

--- Cartorio_Lucas.c
+++ Cartorio_Lucas.c
@@ -128,6 +128,7 @@ int main()
 	    printf("\t1-Registrar nomes\n");
 	    printf("\t2-Consultar nomes\n");
 	    printf("\t3-Deletar nomes\n\n");
+	    printf("\t4-Sair\n\n");
 	    printf("Opção:");//final do menu
 	
     	scanf("%d",&opcao);//armazenamendo escolha do usuario
@@ -148,7 +149,12 @@ int main()
            	case 3:
             deletar();
 		    break;
-		    	
+		    
+		    case 4:
+		    printf("Obrigado por utilizar o sistema!\n");
+		    return 0;
+		    break;
+		    
 		    default:
 			 printf("Essa opção não ésta disponivel\n");
 		    system("pause");
